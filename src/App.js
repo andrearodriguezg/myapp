d26a33cfdf13d79b61a99bcb4481b43fe7bce492
andrearodriguezg <andrearodriguezg95@gmail.com>
2021-12-06 18:53:14 -0500

    primer commit git hub

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+   <a class="navbar-brand js-scroll-trigger" href="#page-top">
+       <span class="d-block d-lg-none">LABORATORIO CLÍNICO</span>
+       <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="%PUBLIC_URL%/assets/img/lab.jpg" alt="" /></span>
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav">
+           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#inicio">Inicio</a></li>
+           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#nosotros">Nuestra empresa</a></li>
+           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contacto">Contacto</a></li>
+           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#agenda">Programa tu cita</a></li>
+           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#descarga">Descarga de resultados</a></li>
+           
+       </ul>
     </div>
-  );
-}
+  </nav>
+  
+ 
+    );
+  }
 
-export default App;
+  export default App;
